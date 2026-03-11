5e07ff129b8be9e4b90851bf07b799b04c031859
Ian Lam <ljy200523@gmail.com>
2026-03-11 16:55:52 +0800

    Progress to step 5
    Writing functions to update number variables
    Ensure to include "event" in .addEventListener
    Ensure to use querySelectorAll() for classes with multiple elements

--- logic.js
+++ logic.js
@@ -14,10 +14,32 @@ function divide(a, b) {
     return a / b;
 }
 
-let a;
-let b;
+let a = [];
+let b = [];
 let operator;
 
 function operate(a, b, operator) {
     return operator(a, b);
+}
+
+const screen = document.querySelector(".screen");
+const buttons = Array.from(document.querySelectorAll(".buttons"));
+const operators = Array.from(document.querySelectorAll(".operators"));
+
+
+function updateVariable() {
+        buttons.addEventListener("click", (event) => {
+        if (operator === null) {
+        screen.textContent = event.target.textContent;
+        a.push(event.target.textContent);
+        }
+        else {
+        screen.textContent = event.target.textContent;
+        b.push(event.target.textContent);
+        }
+    })
+    operators.addEventListener("click", (event) => {
+        screen.textContent = event.target.textContent;
+        operator = event.target.textContent;
+    })
 }
