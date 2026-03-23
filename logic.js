4fba500704f7ac956e50f4dc7dec5a3154f999f2
Ian Lam <ljy200523@gmail.com>
2026-03-23 22:58:23 +0800

    Refactor code for readability

--- logic.js
+++ logic.js
@@ -1,22 +1,19 @@
-function add(a, b) {
-    return a + b;
-}
-
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
 let firstNumber = [];
 let secondNumber = [];
 let operation = null;
+
+const screen = document.querySelector(".screen");
+const buttons = Array.from(document.querySelectorAll(".button"));
+const operators = Array.from(document.querySelectorAll(".operators .operator"));
+const equal = document.querySelector(".equal");
+const clear = document.querySelector(".clear");
+
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
 
 function operate(firstNumber, secondNumber, sign) {
     console.log(`original firstNumber: ${firstNumber}, sign: ${sign}, original secondNumber: ${secondNumber}`);
@@ -29,11 +26,6 @@ function operate(firstNumber, secondNumber, sign) {
     else if (sign === "/") return divide(firstNumber, secondNumber);
 }
 
-const screen = document.querySelector(".screen");
-const buttons = Array.from(document.querySelectorAll(".button"));
-const operators = Array.from(document.querySelectorAll(".operators .operator"));
-const equal = document.querySelector(".equal");
-const clear = document.querySelector(".clear");
 
 function updateVariable() {
     buttons.forEach(button => {
@@ -49,6 +41,7 @@ function updateVariable() {
     });
     operators.forEach(operator => {
         operator.addEventListener("click", (event) => {
+           
             screen.textContent += event.target.textContent;
             operation = event.target.textContent;
         });
